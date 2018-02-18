e082bde86ba869b4344ffbf2d090f9a3c026ea81
David Bertini <david.bertini@gmail.com>
2018-02-18 21:06:04 +0100

    test sulla gestione dei css al posto di gss

---
+++ src/main/java/it/db/budget/client/application/entity/ProdottiEntity.java
@@ -0,0 +1,27 @@
+package it.db.budget.client.application.entity;
+
+import java.io.Serializable;
+
+public class ProdottiEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4668193102790870326L;
+	
+	private Long idProdottoSpesa;
+	private String prodottoSpesa;
+	public Long getIdProdottoSpesa() {
+		return idProdottoSpesa;
+	}
+	public void setIdProdottoSpesa(Long idProdottoSpesa) {
+		this.idProdottoSpesa = idProdottoSpesa;
+	}
+	public String getProdottoSpesa() {
+		return prodottoSpesa;
+	}
+	public void setProdottoSpesa(String prodottoSpesa) {
+		this.prodottoSpesa = prodottoSpesa;
+	}
+
+}
--- src/main/java/it/db/budget/client/application/prodotti/AnagProdottiView.java
+++ src/main/java/it/db/budget/client/application/prodotti/AnagProdottiView.java
@@ -1,37 +1,90 @@
 package it.db.budget.client.application.prodotti;
 
 import org.gwtbootstrap3.client.ui.Button;
+import org.gwtbootstrap3.client.ui.TextBox;
+import org.gwtbootstrap3.client.ui.gwt.CellTable;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.ListDataProvider;
 import com.google.inject.Inject;
 import com.gwtplatform.mvp.client.ViewWithUiHandlers;
+
+import it.db.budget.client.application.entity.ProdottiEntity;
 
 public class AnagProdottiView extends ViewWithUiHandlers<AnagProdottiPresenter> implements AnagProdottiPresenter.MyView {
 
 	
 	@UiField
-	Button indietroButton;
+	Button cercaButton;
+	@UiField
+	Button salvaButton;
+	@UiField
+	TextBox prodottoTB;
 	
+	@UiField(provided = true)
+    CellTable<ProdottiEntity> cellTable = new CellTable<ProdottiEntity>(10);
 	
 	interface Binder extends UiBinder<Widget, AnagProdottiView> {
 	}
+	
+	private ListDataProvider<ProdottiEntity> cellTableProvider = new ListDataProvider<ProdottiEntity>();
 	
 	@Inject
 	AnagProdottiView(Binder uiBinder) {
 		initWidget(uiBinder.createAndBindUi(this));
 		
+		
+		
+		final TextColumn<ProdottiEntity> col1 = new TextColumn<ProdottiEntity>() {
+
+            @Override
+            public String getValue(final ProdottiEntity object) {
+                return String.valueOf(object.getIdProdottoSpesa().toString());
+            }
+        };
+        cellTable.addColumn(col1, "Prodotto");
+		
+        final TextColumn<ProdottiEntity> col2 = new TextColumn<ProdottiEntity>() {
+
+            @Override
+            public String getValue(final ProdottiEntity object) {
+                return String.valueOf(object.getIdProdottoSpesa().toString());
+            }
+        };
+        cellTable.addColumn(col2, "Prodotto");
+        
+        cellTableProvider.addDataDisplay(cellTable);
+        
+        
+        
+        ProdottiEntity prod = new ProdottiEntity();
+        prod.setIdProdottoSpesa(new Long(1));
+        prod.setProdottoSpesa("Pane");
+        
+        
+        
+        cellTableProvider.getList().add(prod);
+        
+		
 		GWT.log("AnagProdottiView load ");
 	}
 
 	
-	@UiHandler("indietroButton")
-	public void onButtonIndietroClick(final ClickEvent event) {
+	@UiHandler("salvaButton")
+	public void onSalvaButtonClick(final ClickEvent event) {
 		//getUiHandlers().sendToListaDataSource();
-		GWT.log("Cliccato bottone!");
+		GWT.log("Cliccato bottone Salva!");
+	}
+	
+	@UiHandler("cercaButton")
+	public void onCercaButtonClick(final ClickEvent event) {
+		//getUiHandlers().sendToListaDataSource();
+		GWT.log("Cliccato bottone cerca!");
 	}
 }
